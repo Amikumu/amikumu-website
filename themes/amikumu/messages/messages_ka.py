3c7b0036f9d6f8b148fa7395d65a03180c354567
Tanja <GitHub@tannie.nl>
2022-11-11 20:02:30 +0100

    updated messages_ka

--- themes/amikumu/messages/messages_ka.py
+++ themes/amikumu/messages/messages_ka.py
@@ -53,7 +53,6 @@ MESSAGES =  {
     "Updates": "Updates",
     "Write your page here.": "Write your page here.",
     "Write your post here.": "Write your post here.",
-    "about_us": "About us",
     "old posts, page %d": "old posts, page %d",
     "page %d": "page %d",
     "updated": "updated"
